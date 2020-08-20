d61117023117fce58d187178492b402109e5aa81
prakhil-tp <prakhil.tp@gmail.com>
2020-08-21 02:07:39 +0530

    feat: Add buyer and sellter page templates

---
+++ src/pages/Buyer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Buyer = (): JSX.Element => {
+  return (
+    <div>
+      <h1>This buyer screen</h1>{" "}
+    </div>
+  );
+};
+
+export default Buyer;
---
+++ src/pages/Seller/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Seller = (): JSX.Element => {
+  return (
+    <div>
+      <h1>This is seller screen</h1>
+    </div>
+  );
+};
+
+export default Seller;
