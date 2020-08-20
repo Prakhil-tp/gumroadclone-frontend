4ca183f345ed499d0c8c7312c33d4df7d35f0b04
prakhil-tp <prakhil.tp@gmail.com>
2020-08-21 02:11:58 +0530

    feat #1: Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Auth from "./pages/Auth";
+import Buyer from "./pages/Buyer";
+import Seller from "./pages/Seller";
+
+function checkLoginStatus(): boolean {
+  return false;
+}
 
 function App(): JSX.Element {
-  return <h1> hey there.</h1>;
+  const [isLogin, setLogin] = useState<boolean>();
+
+  useEffect(() => {
+    const status = checkLoginStatus();
+    setLogin(status);
+  }, []);
+
+  if (isLogin === undefined) {
+    return <h1>Loading..</h1>;
+  }
+
+  return (
+    <Router>
+      <Switch>
+        {!isLogin && <Route exact path="/auth" component={Auth} />}
+        {!isLogin && <Redirect to="/auth" />}
+        <Route path="/dashboard" component={Seller} />
+        <Route path="/" component={Buyer} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
