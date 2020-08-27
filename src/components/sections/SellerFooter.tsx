241a18177b45d1f1d5971810d6fca79e79d59b1c
prakhil-tp <prakhil.tp@gmail.com>
2020-08-27 22:47:04 +0530

    feat: Add SellerFooter #5

--- src/components/sections/SellerFooter.tsx
+++ src/components/sections/SellerFooter.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Flex, Text, HStack } from "@chakra-ui/core";
 
-const SellerFooter = (): JSX.Element => {
-  return <div />;
+const SellerFooter: React.FC = () => {
+  return (
+    <Flex
+      w="100%"
+      h="auto"
+      justifyContent="space-between"
+      p="30px"
+      fontSize="13px"
+      borderTop="1px solid #e3e3e3"
+    >
+      <HStack color="gray.700">
+        <Text color="gray.500">@2020</Text>
+        <Text>Vonnue, Inc</Text>
+      </HStack>
+      <HStack color="gray.500">
+        <Text>@vonnue</Text>
+        <Text>Help</Text>
+        <Text>Privacy Policy</Text>
+        <Text>Terms of use</Text>
+      </HStack>
+    </Flex>
+  );
 };
 
 export default SellerFooter;
