f1afca88ac439b2558ef53241277de6a2819c20b
prakhil-tp <prakhil.tp@gmail.com>
2020-08-29 22:40:34 +0530

    feat: Boilerplate for DetailsForm

---
+++ src/pages/Seller/components/ProductForm/DetailsForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Product } from "types";
+
+type Props = {
+  currentProduct: Product;
+};
+
+const DetailsForm = ({ currentProduct }: Props) => {
+  return <div />;
+};
+
+export default DetailsForm;
