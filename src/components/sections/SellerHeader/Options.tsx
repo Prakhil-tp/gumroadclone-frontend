733777d6c36c79b04cd1a42cf6583a230b9ed02d
prakhil-tp <prakhil.tp@gmail.com>
2020-08-29 22:30:23 +0530

    Fix: Change flex to HStack, alignment fix

---
+++ src/components/sections/SellerHeader/Options.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useCallback } from "react";
+import { Box, Text, HStack } from "@chakra-ui/core";
+import ClassNames from "classnames";
+import styles from "./Options.module.scss";
+
+const Options = () => {
+  const [currentTab, setCurrentTab] = useState("Products");
+
+  const boxStyle = useCallback((option: string, tab = "Products") => {
+    return ClassNames(styles.NavOptions, {
+      [styles["NavOptions--active"]]: tab === option
+    });
+  }, []);
+
+  const handleOptionsClick = (e: React.MouseEvent) => {
+    setCurrentTab(e.currentTarget.textContent || "");
+  };
+
+  return (
+    <HStack
+      p="0px 0px 0px 0px"
+      spacing="28px"
+      justifyContent="center"
+      fontSize="13px"
+      fontWeight="700"
+      color="text.gray"
+      w="100%"
+    >
+      <Box
+        className={boxStyle("Products", currentTab)}
+        onClick={handleOptionsClick}
+      >
+        <i className="fa fa-archive" />
+        <Text>Products</Text>
+      </Box>
+      <Box
+        className={boxStyle("Customers", currentTab)}
+        onClick={handleOptionsClick}
+      >
+        <i className="fas fa-user-friends" />
+        <Text>Customers</Text>
+      </Box>
+      <Box
+        className={boxStyle("Posts", currentTab)}
+        onClick={handleOptionsClick}
+      >
+        <i className="fa fa-newspaper" />
+        <Text>Posts</Text>
+      </Box>
+      <Box
+        className={boxStyle("Analytics", currentTab)}
+        onClick={handleOptionsClick}
+      >
+        <i className="fas fa-chart-bar" />
+        <Text>Analytics</Text>
+      </Box>
+      <Box
+        className={boxStyle("Profile", currentTab)}
+        onClick={handleOptionsClick}
+      >
+        <i className="fa fa-store" />
+        <Text>Profile</Text>
+      </Box>
+    </HStack>
+  );
+};
+
+export default Options;
