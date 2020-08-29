4b815e21e5e45d2643229f09ffe459d5d88ae3a7
prakhil-tp <prakhil.tp@gmail.com>
2020-08-29 22:29:10 +0530

    feat: Add common Product type

---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export type Product = {
+  id: string;
+  title: string;
+  description?: string;
+  sales: string | number;
+  total: string | number;
+  price: string | number;
+  type: "classic" | "membership";
+};
