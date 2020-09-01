7241dbb2affb5947e98ace16e2de11a3a4926664
prakhil-tp <prakhil.tp@gmail.com>
2020-09-01 22:04:23 +0530

    feat: Make HStackButtons selectable #5

--- src/components/ui/HStackButton.tsx
+++ src/components/ui/HStackButton.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { HStack } from "@chakra-ui/core";
 
-const HStackButton: React.FC = ({ children }) => {
+type Props = {
+  selected?: boolean;
+  onClick: () => void;
+};
+const HStackButton: React.FC<Props> = ({ children, selected, onClick }) => {
   return (
     <HStack
       w="100%"
       p="15px"
       spacing="2"
-      border="1px solid #ddd"
       color="text.gray"
       borderRadius="7px"
+      border={`1px solid ${selected ? "#36a9ae" : "#ddd"}`}
+      boxShadow={selected ? "inset 0 0 0 1px #36a9ae" : ""}
+      onClick={onClick}
     >
       {children}
     </HStack>
