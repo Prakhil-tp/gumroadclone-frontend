76e3ae8b9cc9e4ee3fbc9392660f3ba15b33f490
prakhil-tp <prakhil.tp@gmail.com>
2020-09-01 22:12:41 +0530

    feat: Add a condition to route the detailsForm from the membershipForm

--- src/pages/Seller/components/ProductForm/index.tsx
+++ src/pages/Seller/components/ProductForm/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useProducts } from "contexts/SellerProductContext";
+import { useProductForm } from "contexts/ProductFormContext";
 import DetailsForm from "./DetailsForm";
 import MembershipForm from "./MembershipForm";
 
 const ProductForm = () => {
   const { currentProductId, products } = useProducts();
+  const { currentForm, currentProduct: productDetails } = useProductForm();
   let currentProduct;
 
   if (currentProductId) {
     currentProduct = products.find(
       (product) => product.id === currentProductId
     );
+  } else if (currentForm === "DetailsForm") {
+    currentProduct = productDetails;
   }
 
   return currentProduct ? (
