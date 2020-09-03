f042da438288d843f3d4de836c6ddb359c017202
prakhil-tp <prakhil.tp@gmail.com>
2020-09-03 23:56:04 +0530

    feat: Add boilerplate for BuyerHeader #4

---
+++ src/components/sections/BuyerHeader/BuyerHeader.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Grid } from "@chakra-ui/core";
+
+const BuyerHeader = () => {
+  return (
+    <Grid
+      w="100%"
+      p="20px"
+      bg="white"
+      templateRows="1"
+      borderTop="2px solid #1b5557"
+      borderBottom="1px solid #ddd"
+    >
+      Nav Bar
+    </Grid>
+  );
+};
+
+export default BuyerHeader;
