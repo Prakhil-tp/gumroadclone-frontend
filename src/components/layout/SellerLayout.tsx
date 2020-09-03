e50902ecb62f43e69e87c434ed219f60953a306e
prakhil-tp <prakhil.tp@gmail.com>
2020-09-03 23:54:58 +0530

    refactor: Rename Footer

--- src/components/layout/SellerLayout.tsx
+++ src/components/layout/SellerLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Flex } from "@chakra-ui/core";
 import Header from "../sections/SellerHeader";
-import Footer from "../sections/SellerFooter";
+import Footer from "../sections/Footer";
 
 const SellerLayout: React.FC = ({ children }) => (
   <Flex direction="column" align="center" maxW={{ xl: "1980px" }} m="0 auto">
