a06d709227a9670bd574058d2d0e792a2647b225
prakhil-tp <prakhil.tp@gmail.com>
2020-09-03 23:53:56 +0530

    feat: Add BuyerLayout

--- src/components/layout/BuyerLayout.tsx
+++ src/components/layout/BuyerLayout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Flex } from "@chakra-ui/core";
+import Header from "../sections/BuyerHeader";
+import Footer from "../sections/Footer";
 
-const BuyerLayout = (): JSX.Element => {
-  return <div />;
-};
-
+const BuyerLayout: React.FC = ({ children }) => (
+  <Flex direction="column" align="center" maxW={{ xl: "1980px" }} m="0 auto">
+    <Header />
+    {children}
+    <Footer />
+  </Flex>
+);
 export default BuyerLayout;
