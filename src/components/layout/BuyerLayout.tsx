264191a5d0bfb0b6afb7c87036b02f4b235ef3b2
prakhil-tp <prakhil.tp@gmail.com>
2020-09-05 22:34:50 +0530

    feat: implement buyerview switching #4

--- src/components/layout/BuyerLayout.tsx
+++ src/components/layout/BuyerLayout.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-import { Flex } from "@chakra-ui/core";
+import React, { useState } from "react";
+import { Flex, Box } from "@chakra-ui/core";
+import { BuyerScreens } from "types";
 import Header from "../sections/BuyerHeader";
 import Footer from "../sections/Footer";
 
-const BuyerLayout: React.FC = ({ children }) => (
-  <Flex direction="column" align="center" maxW={{ xl: "1980px" }} m="0 auto">
-    <Header />
-    {children}
-    <Footer />
-  </Flex>
-);
+type Props = {
+  children: JSX.Element[];
+};
+
+const BuyerLayout: React.FC<Props> = ({ children }) => {
+  const [currentView, setCurrentView] = useState<BuyerScreens>("Discover");
+  return (
+    <Flex direction="column" align="center" maxW={{ xl: "1980px" }} m="0 auto">
+      <Header currentView={currentView} setCurrentView={setCurrentView} />
+      <Box p="100px 50px" w="100%">
+        {currentView === "Discover" && children[0]}
+        {currentView === "Search" && children[1]}
+        {currentView === "Library" && children[2]}
+      </Box>
+      <Footer />
+    </Flex>
+  );
+};
 export default BuyerLayout;
