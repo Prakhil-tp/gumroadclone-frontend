22e6e0f7b59eef9c1aed532404ad3de02204f54c
prakhil-tp <prakhil.tp@gmail.com>
2020-09-08 05:31:45 +0530

    fix: Amend ui component exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,4 @@
 export { default as HStackButton } from "./HStackButton";
 export { default as Label } from "./Label";
+export { default as ExploreCard } from "./ExploreCard";
+export { default as ProductCard } from "./ProductCard";
