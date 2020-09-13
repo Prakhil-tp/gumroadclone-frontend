65e3b32cfab4d4faba6d4db3c7fa2c2835f39ca2
prakhil-tp <prakhil.tp@gmail.com>
2020-09-14 03:14:18 +0530

    feat: Add menu to the avatar

--- src/components/sections/SellerHeader/SellerHeader.tsx
+++ src/components/sections/SellerHeader/SellerHeader.tsx
@@ -1,58 +1,51 @@
 import React from "react";
-import { Box, Grid, Flex } from "@chakra-ui/core";
+import { useHistory } from "react-router-dom";
+import {
+  Box,
+  Grid,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem
+} from "@chakra-ui/core";
 import Options from "./Options";
 
-const SellerHeader = (): JSX.Element => {
-  const colors = [
-    "#137e83",
-    "#006c71",
-    "#f4b936",
-    "#f29900",
-    "#e96b5a",
-    "#ed5741",
-    "#cb283e",
-    "#a71a28",
-    "#03949a"
-  ];
-  const getHeadLiner = () => {
-    const boxes = [];
-    for (let i = 0; i < 13; i++) {
-      const item = <Box key={i} bg={colors[i % colors.length]} />;
-      boxes.push(item);
-    }
-    return boxes;
-  };
+const SellerHeader = () => {
+  const history = useHistory();
 
   return (
-    <>
-      <Grid w="100%" h="2px" templateColumns="repeat(13,1fr)" templateRows="1">
-        {getHeadLiner()}
-      </Grid>
-      <Grid
-        w="100%"
-        p="21px 30px 16px 30px"
-        bg="background.grey"
-        templateColumns={{
-          sm: "1fr 4fr 1fr",
-          md: "1fr 4fr 1fr",
-          lg: "1fr 2fr 1fr",
-          xl: "1fr 1.5fr 1fr"
-        }}
-        templateRows="1"
-        borderBottom="1px solid #e3e3e3"
-      >
-        <Box>
-          <img src="/assets/logo.svg" alt="logo" style={{ maxWidth: "24px" }} />
-        </Box>
-        <Options />
-        <Flex direction="row" justifyContent="flex-end">
-          <i
-            className="fa fa-user-circle"
-            style={{ fontSize: "30px", color: "#797874" }}
-          />
-        </Flex>
-      </Grid>
-    </>
+    <Grid
+      w="100%"
+      p="21px 30px 16px 30px"
+      bg="background.grey"
+      templateColumns={{
+        sm: "1fr 4fr 1fr",
+        md: "1fr 4fr 1fr",
+        lg: "1fr 2fr 1fr",
+        xl: "1fr 1.5fr 1fr"
+      }}
+      templateRows="1"
+      borderBottom="1px solid #e3e3e3"
+    >
+      <Box>
+        <img src="/assets/logo.svg" alt="logo" style={{ maxWidth: "24px" }} />
+      </Box>
+      <Options />
+      <Flex direction="row" justifyContent="flex-end">
+        <Menu>
+          <MenuButton fontSize="30px" color="icon.gray">
+            <i className="fa fa-user-circle" style={{ color: "inherit" }} />
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={() => history.goBack()}>
+              Switch to buying
+            </MenuItem>
+            <MenuItem>logout</MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+    </Grid>
   );
 };
 
