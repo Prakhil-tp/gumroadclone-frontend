27ffeb32246a8444c2cf84e1fe2ff6962a6c6071
prakhil-tp <prakhil.tp@gmail.com>
2020-09-14 03:22:53 +0530

    feat: Add ProductCard grid on Library view #4

--- src/pages/Buyer/Library.tsx
+++ src/pages/Buyer/Library.tsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { Grid } from "@chakra-ui/core";
+import { ProductCard } from "components/ui";
 
 const Library = () => {
-  return <div />;
+  return (
+    <Grid
+      paddingTop="50px"
+      gap={6}
+      templateColumns="repeat(auto-fit,minmax(300px,300px))"
+    >
+      <ProductCard
+        key="3"
+        title="this is titile"
+        owner={{ name: "babu" }}
+        price="34"
+        cover="assets/images/product-card-image.png"
+      />
+      <ProductCard
+        key="5"
+        title="this is titile"
+        owner={{ name: "babu" }}
+        price="34"
+        cover="assets/images/product-card-image.png"
+      />
+      <ProductCard
+        key="8"
+        title="this is titile"
+        owner={{ name: "babu" }}
+        price="34"
+        cover="assets/images/product-card-image.png"
+      />
+      <ProductCard
+        key="4"
+        title="this is titile"
+        owner={{ name: "babu" }}
+        price="34"
+        cover="assets/images/product-card-image.png"
+      />
+      <ProductCard
+        key="7"
+        title="this is titile"
+        owner={{ name: "babu" }}
+        price="34"
+        cover="assets/images/product-card-image.png"
+      />
+      <ProductCard
+        key="6"
+        title="this is titile"
+        owner={{ name: "babu" }}
+        price="34"
+        cover="assets/images/product-card-image.png"
+      />
+    </Grid>
+  );
 };
 
 export default Library;
